fd5d4588ca36b1d7ea354fe9e05f87e88f357c37
TWVerstraaten <t.w.verstraaten@rug.nl>
2021-01-08 01:40:14 +0100

    Removed nonsense from main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,6 @@
 #include "app/Application.h"
 
 int main(int argc, char* argv[]) {
-
-    //    model::CommandVector c{{"LFT 1", "RHT 2", " ### ", "FWD", "BCK 2 "}};
-    //
-    //    for (size_t i = 0; i != 8; ++i) {
-    //        std::cout << model::CommandParser::toString(c.currentCommand()) << '\n';
-    //        c.increment();
-    //    }
-
     app::Application applicationLevel;
     applicationLevel.run();
 
