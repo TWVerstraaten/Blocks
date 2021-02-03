855a53fb3227bef50962686978a0613a2eaf746b
TWVerstraaten <t.w.verstraaten@rug.nl>
2021-02-03 13:28:54 +0100

    Removed libsoundio at ThridParty library

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QApplication>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+
     QApplication app(argc, argv);
     auto         window = view::widget::Window();
     window.show();
